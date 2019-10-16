54019a98b2550b0718ec95d3e485c366eabaa400
Calvin Seamons <calvindseamons@lanl.gov>
2019-10-16 12:04:11 -0600

    working pio spack package

--- var/spack/repos/builtin/packages/parallelio/package.py
+++ var/spack/repos/builtin/packages/parallelio/package.py
@@ -22,23 +22,22 @@ class Parallelio(AutotoolsPackage):
     version('2.4.2', sha256='55e7c0d8911139599e2f31b1aa79bf5e0f2818d2519ae40671db5711977afcc6')
     version('2.4.1', sha256='f3ca3f56aa18efa1c4d7cdc6ce1c7b11d3f6382d31ec96874897814dffe6a78a')
 
-    variant('full',    default=False, description='If not relying on packages already built use "+full"')
-    variant('fortran', default=False, description='Builds with netcdf-fortran')
-    variant('shared',  default=False, description='Enable shared')
+    #variant('fortran', default=False, description='Builds with netcdf-fortran')
+    #variant('shared',  default=False, description='Enable shared')
+
+    #currently building everything needed for this horrid thing to work.
 
     depends_on('zlib@1.2.11')
-    depends_on('openmpi@3.1.2',                                   type='build', when='+full')
-    depends_on('python@2.7',                                      type='build', when='+full')
-    depends_on('hdf5@1.10.5~cxx+fortranio+shared+mpi+hl+vfd+all', type='build', when='+full')
-    depends_on('parallel-netcdf+shared',                          type='build', when='+full')
-    depends_on('netcdf@4.7.0+parallel-netcdf+mpi+shared',         type='build', when='+full')
-    depends_on('netcdf-fortran',                                  type='build', when='+full')
+    depends_on('openmpi@3.1.2')
+    depends_on('python@2.7')
+    depends_on('hdf5@1.10.5~cxx+fortran-io+shared+mpi+hl+vfd+fortran')
+    depends_on('parallel-netcdf+shared~cxx')
+    depends_on('netcdf@4.7.0+parallel-netcdf+mpi+shared')
+    depends_on('netcdf-fortran')
 
     def configure_args(self):
         spec = self.spec
-        rpath = self.rpath
         args = []
-        #cflags = []
         cppflags = []
         ldflags = []
 
@@ -52,20 +51,16 @@ class Parallelio(AutotoolsPackage):
         #ldflags.append(hdf5_hl.libs.search_flags)
         #ldflags.insert(0,'-L')
         #cppflags.insert(0,'-I')
-        netcdf = ('%s' % self.spec['netcdf'].prefix)
-        ldflags.append(netcdf+'/lib -lnetcdf')
-        if '+full' in spec:
-            args.append('CC=%s' % self.spec['openmpi'].prefix +'/bin/mpicc')
-            args.append('FC=mpif90')
-            args.append('CFLAG=-std=c99')
+        #netcdf = ('%s' % self.spec['netcdf'].prefix)
+        #ldflags.append(netcdf+'/lib -lnetcdf')
+        args.append('CC=%s' % self.spec['openmpi'].prefix +'/bin/mpicc')
+        args.append('FC=mpif90')
+        args.append('CFLAG=-std=c99')
 
-        if '+fortran' in spec:
-            args.append('--enable-fortran')
-        if '+shared' in spec:
-            args.append('--enable-shared')
+        args.append('--enable-fortran')
+        args.append('--enable-shared')
 
-        #args.append('CFLAGS=' + ' '.join(cflags))
-        args.append('CPPFLAGS=' + ' '.join(cppflags))
-        args.append('LDFLAGS=' + ' '.join(ldflags))
+        #args.append('CPPFLAGS=' + ' '.join(cppflags))
+        #args.append('LDFLAGS=' + ' '.join(ldflags))
         return args
 
